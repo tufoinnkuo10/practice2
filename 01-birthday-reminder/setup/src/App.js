478b2e5ee75cfef5285000639ab097f4598cf782
Tufoin Nkuo <tufoinnkuo10@gmail.com>
2022-05-05 18:31:07 +0100

    add remove all and remove button

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -2,11 +2,12 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
+  const[people, setPeople] = useState(data)
   return <main>
     <section className='container'>
-      <h3>0 birthdays today</h3>
-      <List />
-      <button onClick={()=> console.log('you have clicked')}>
+      <h3>{people.length} birthdays today</h3>
+      <List people={people} setPeople={setPeople}/>
+      <button onClick={()=>setPeople([])}>
         clear all
       </button>
     </section>
